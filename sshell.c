e545059e6e13c05e2395da8c69e36885ae00c668
Michel Eter <meter@ucdavis.edu>
2020-01-10 23:25:00 -0800

    Added skeleton for 'cd' builtin

--- sshell.c
+++ sshell.c
@@ -19,6 +19,7 @@ int main(void)
     {
         char *nl;
         int retval;
+        char *fxn;
 
         // Print prompt
         printf("sshell$ ");
@@ -26,6 +27,11 @@ int main(void)
 
         // Get command line
         fgets(cmd, CMDLINE_MAX, stdin);
+
+        // Sets 'fxn' equal to first input, if cmd is more than one arg
+        // fxn = calloc(strlen(cmd) + 1, sizeof(char));
+        // strcpy(fxn, cmd);
+        // strtok(fxn, " ");
 
         // Print command line if stdin is not provided by terminal
         if (!isatty(STDIN_FILENO))
@@ -54,6 +60,20 @@ int main(void)
             retval = system(cmd);
             print_completion(cmd, retval);
         }
+
+        // Builtin 'cd' command
+        // if (!strcmp(fxn, "cd"))
+        // {
+        //     retval = system(cmd);
+        //     if (!retval)
+        //     {
+        //         print_completion(cmd, retval);
+        //     }
+        //     else
+        //     {
+        //         printf("error\n");
+        //     }
+        // }
 
         // Regular command
         // retval = system(cmd);
