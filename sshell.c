2c3f174f9385a03a64fddd60b06dda3ea72abfed
Michel Eter <meter@ucdavis.edu>
2020-01-15 19:28:45 -0800

    Started char parsing

--- sshell.c
+++ sshell.c
@@ -19,25 +19,50 @@ struct Command
         // char *fileName;
 } Command;
 
+// char **setPrefix(struct Command *command, char *cmdStr)
+// {
+//         char **leftOff;
+//         command->prefix = strtok_r(cmdStr, " ", leftOff);
+//         return leftOff;
+// }
+
 struct Command *parseCommand(char *cmdStr)
 {
         struct Command *command = malloc(sizeof(struct Command));
-        // char* nr;
 
-        int i = 1;
-        char *cur = strtok(cmdStr, " ");
-        command->prefix = cur;
-        command->args[0] = command->prefix;
-
-        while (cur)
+        for (u_int i = 0, j = 0; i < strlen(cmdStr); i++)
         {
-                cur = strtok(NULL, " ");
-
-                command->args[i] = cur;
-
-                i++;
+                if (cmdStr[i] != ' ')
+                {
+                        // Either a meta-char or normal char
+                        if (cmdStr[i] == '>')
+                        {
+                                // if (cmdStr[i + 1] == '&')
+                                // {
+                                // }
+                        }
+                        else if (cmdStr[i] == '|')
+                        {
+                                //                 // if (cmdStr[i + 1] == '&')
+                                //                 // {
+                                //                 // }
+                        }
+                        else
+                        {
+                                // Normal char
+                                printf("Normal\n");
+                                char *tmp = strcat(command->args[j], &cmdStr[i]);
+                                // command->args[j] = "testing";
+                                printf("%s\n", tmp);
+                        }
+                }
+                else
+                {
+                        // Space found
+                        printf("Space\n");
+                        j += 1;
+                }
         }
-        command->args[i] = NULL;
 
         return command;
 }
@@ -55,15 +80,10 @@ int main(void)
         while (1)
         {
                 char *nl;
-                char *nr;
-                char *cur;
-                // char *tmp;
                 char fxn[CMDLINE_MAX];
-                int retval;
-                int status;
-                // int fd;
-                pid_t pid;
-                // bool needsRedirection = false;
+                // int retval;
+                // int status;
+                // pid_t pid;
 
                 /* Print prompt */
                 printf("sshell$ ");
@@ -85,88 +105,75 @@ int main(void)
                 if (nl)
                         *nl = '\0';
 
-                /* Finds output redirection character */
+                /* Create copy of 'cmd' */
                 strcpy(fxn, cmd);
-                nr = strchr(fxn, '>');
-                // int count = 1;
-                if (nr) {
-                        cur = strtok(fxn, ">");
-                        // printf("%s\n", cur);
-                        while (cur) {
-                          cur = strtok(NULL, ">");
-                        }
-
-                }
-                else {
-                  command = parseCommand(fxn);
-                }
-
-
+                command = parseCommand(fxn);
+                command = command;
 
                 /* Builtin commands */
-                if (!strcmp(command->prefix, "exit"))
-                {
-                        fprintf(stderr, "Bye...\n");
-                        print_completion(cmd, errno);
-                        break;
-                }
-                else if (!strcmp(cmd, "pwd"))
-                {
-                        char buf[CMDLINE_MAX];
-                        char *dir = getcwd(buf, (size_t)CMDLINE_MAX);
-                        if (dir)
-                        {
-                                printf("%s\n", dir);
-                                print_completion(cmd, errno);
-                        }
-                        else
-                        {
-                                perror("getcwd");
-                        }
-                }
-                else if (!strcmp(command->prefix, "cd"))
-                {
-                  			if (command->args[1] != NULL)
-                  			{
-                                retval = chdir(command->args[1]);
-                                if (!retval)
-                                {
-                                       print_completion(cmd, retval);
-                                }
-                  				      else
-                  				      {
-                  					           perror("chdir");
-                  				      }
-                  			}
-                  			else
-                  			{
-                  				fprintf(stderr, "Null directory\n");
-                  			}
-                }
-                else
-                {
-                        /* Regular command */
-                        pid = fork();
-                        if (pid == 0)
-                        {
-                                // Child
-                                execvp(command->prefix, command->args);
-                                perror("execvp");
-                                exit(1);
-                        }
-                        else if (pid > 0)
-                        {
-                                // Parent
-                                waitpid(-1, &status, 0);
-                                print_completion(cmd, status);
-                        }
-                        else
-                        {
-                                // Error
-                                perror("fork");
-                                exit(1);
-                        }
-                }
+                //         if (!strcmp(command->prefix, "exit"))
+                //         {
+                //                 fprintf(stderr, "Bye...\n");
+                //                 print_completion(cmd, errno);
+                //                 break;
+                //         }
+                //         else if (!strcmp(cmd, "pwd"))
+                //         {
+                //                 char buf[CMDLINE_MAX];
+                //                 char *dir = getcwd(buf, (size_t)CMDLINE_MAX);
+                //                 if (dir)
+                //                 {
+                //                         printf("%s\n", dir);
+                //                         print_completion(cmd, errno);
+                //                 }
+                //                 else
+                //                 {
+                //                         perror("getcwd");
+                //                 }
+                //         }
+                //         else if (!strcmp(command->prefix, "cd"))
+                //         {
+                //                 if (command->args[1] != NULL)
+                //                 {
+                //                         retval = chdir(command->args[1]);
+                //                         if (!retval)
+                //                         {
+                //                                 print_completion(cmd, retval);
+                //                         }
+                //                         else
+                //                         {
+                //                                 perror("chdir");
+                //                         }
+                //                 }
+                //                 else
+                //                 {
+                //                         fprintf(stderr, "Null directory\n");
+                //                 }
+                //         }
+                //         else
+                //         {
+                //                 /* Regular command */
+                //                 pid = fork();
+                //                 if (pid == 0)
+                //                 {
+                //                         // Child
+                //                         execvp(command->prefix, command->args);
+                //                         perror("execvp");
+                //                         exit(1);
+                //                 }
+                //                 else if (pid > 0)
+                //                 {
+                //                         // Parent
+                //                         waitpid(-1, &status, 0);
+                //                         print_completion(cmd, status);
+                //                 }
+                //                 else
+                //                 {
+                //                         // Error
+                //                         perror("fork");
+                //                         exit(1);
+                //                 }
+                //         }
         }
 
         return EXIT_SUCCESS;
